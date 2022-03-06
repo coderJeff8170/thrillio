a7f5b7b75ea980a4697126487db2ac4239a3fb68
Cognizant <cognizant@Jeffreys-MacBook-Pro.local>
2022-03-06 00:26:32 -0500

    added constants and data files

---
+++ src/constants/Gender.java
@@ -0,0 +1,12 @@
+package constants;
+
+/**
+ * @author cognizant
+ */
+public class Gender {
+    private Gender() {}
+
+    private static final int MALE = 0;
+    private static final int FEMALE = 1;
+    private static final int TRANSGENDER = 2;
+}
